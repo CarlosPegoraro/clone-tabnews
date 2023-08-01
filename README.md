5d4fdd343f301483320b8e162d82ff93fd6a8965
Carlos - Work <carlospegorarolopes@gmail.com>
2023-08-01 00:25:57 -0300

    adiciona o lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 Um projeto que tem como objetivor criar um clone do <a href="https://www.tabnews.com.br">TabNews</a> para fins de estudo e melhora na minha capacidade como desenvolvedor
 O projeto é ideia do Filipe Deschamps usando a sua plataforma <a href="https://www.curso.dev">curso.dev</a> para trazer passo a passo da criação desse projeto
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-    return (
-        <h1>Oi minha Rosa 🌹 Eu amo muito você ❤️ Se você me ama, me manda uma mensagem assim "Arroz é muito bom, meu trevo"</h1>
-    )
+  return (
+    <h1>
+      Oi minha Rosa 🌹 Eu amo muito você ❤️ Se você me ama, me manda uma
+      mensagem assim "Arroz é muito bom, meu trevo"
+    </h1>
+  );
 }
 
 export default Home;
