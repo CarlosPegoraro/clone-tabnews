3da6f042ba6a66e5da037d460092761fe65ca963
kadu_dev <carlospegorarolopes@gmail.com>
2024-01-01 23:36:35 -0300

    pra mamae 2

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 function Home() {
   return (
     <div>
-      <h1>Feliz 42 anos pra mãe mais fantástica desse mundoooo!!!!! </h1>
+      <h1 style={{color: '#ffab00'}}>Feliz 42 anos pra mãe mais fantástica desse mundoooo!!!!! </h1>
 
-      <p> 
+      <p style={{fontFamily: 'Arial'}}> 
 
         Eu amo tanto você e não tenho palavras pra dizer o quão especial você eh pra mim!!! Minha companheira de séries, minha confidente, minha melhor amiga. Você merece tudo de bom nessa vida, e com toda certeza do mundo vai ter muitoooooo sucesso esse ano e vai realizar todas as suas metas, e eu vou estar aqui sempre pra te ajudar!! <br/>
 
-        Eu amo muitoooooooo você mãe, você eh a melhor que alguém poderia desejar ter!!!!!<br/>
+        <br/>Eu amo muitoooooooo você mãe, você eh a melhor que alguém poderia desejar ter!!!!!<br/>
 
-        Eu amo muitoooooooo você!!!!!! <br/>
+        <br/><span style={{color: '#ff0000'}}>Eu amo muitoooooooo você!!!!!!</span> <br/>
       </p>
     </div>
     
