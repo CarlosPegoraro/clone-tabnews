d3196a4cd47f1b1cbcdce587b1b917839bc00e58
WhiteLinux <carlospegorarolopes@gmail.com>
2026-07-14 23:02:22 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
-test("get to /api/v1/status should return 200", async () => {
+test("get to /api/v1/status should return validated json", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  // validate updated_at prop
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // validate postgres_version prop
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
+
+test.skip("SQL Injection Test", async () => {
+  await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4);--");
 });
