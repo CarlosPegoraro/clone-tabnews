ddad32465b748e7b5507130c5a78f2adbfb42581
WhiteLinux <carlospegorarolopes@gmail.com>
2026-08-03 01:39:22 -0300

    **out class** adjusts in compose.prod.yaml

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.POSTGRES_SSLMODE === "required"
+    ssl: process.env.POSTGRES_SSLMODE === "require"
   });
 
   try {
